12d16be64bcc0f705a1ae4d95ecbe608821d7cdf
rwayda <salahrwayda@gmail.com>
2025-11-21 20:54:59 +0200

    moutputs

---
+++ outputs.tf
@@ -0,0 +1,18 @@
+output "backend_public_ip" {
+  description = "Backend instance public IP"
+  value       = aws_instance.backend.public_ip
+}
+
+output "frontend_public_ip" {
+  description = "Frontend instance public IP"
+  value       = aws_instance.frontend.public_ip
+}
+
+output "rds_endpoint" {
+  description = "RDS endpoint"
+  value       = aws_db_instance.mysql.endpoint
+}
+
+output "sns_topic_arn" {
+  value = aws_sns_topic.alarms.arn
+}
