e1597998005f0798e264803153181604a4ab05e7
rwayda <salahrwayda@gmail.com>
2025-11-21 20:50:31 +0200

    create variables

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "backend_subnet_id" {
+  description = "Subnet for backend"
+  type        = string
+}
+
+variable "frontend_subnet_id" {
+  description = "Subnet for frontend"
+  type        = string
+}
+variable "db_username" {
+  description = "RDS master username"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "RDS master password"
+  type        = string
+  sensitive   = true
+  default     = "MyRds123451!"
+}
+variable "admin_email" {
+  description = "Email to receive alarm notifications"
+  type        = string
+  default     = "rwaydasalah98@gmail.com"
+}
