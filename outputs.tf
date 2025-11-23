b95f37401cb7479dd9e1bfecc232a0e7f213ffc6
rwayda <salahrwayda@gmail.com>
2025-11-23 16:26:47 +0200

    edite

--- outputs.tf
+++ outputs.tf
@@ -16,3 +16,11 @@ output "rds_endpoint" {
 output "sns_topic_arn" {
   value = aws_sns_topic.alarms.arn
 }
+output "frontend_ec2_id" {
+  value = aws_instance.frontend.id
+}
+
+
+output "backend_ec2_id" {
+  value = aws_instance.backend.id
+}
